e07cd72134d7bdb2af1b96b9b2d9b3ce8079d403
Jerko Josipović <81383934+jerko379@users.noreply.github.com>
2021-06-09 13:26:43 +0200

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,8 +1,9 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$basename = "projekt";
+$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$servername = $cleardb_url["host"];
+$username = $cleardb_url["user"];
+$password = $cleardb_url["pass"];
+$basename = substr($cleardb_url["path"],1);
 $dbc = mysqli_connect($servername, $username, $password, $basename) or die('Error
 connecting to MySQL server.'.mysqli_error($dbc));
 mysqli_set_charset($dbc, "utf8");
